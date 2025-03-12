2da1571be8207d585b24fea4c52ef7af79d90064
Даукша Александр Юрьевич <aj.dauksha@yandex.ru>
2025-03-12 15:48:36 +0300

    add base task logic

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        TaskManager manager = new TaskManager();
+
+        Task task1 = manager.createTask(new Task(manager.getNextId(), "Задача 1", "Описание задачи 1", Status.NEW));
+        Task task2 = manager.createTask(new Task(manager.getNextId(), "Задача 2", "Описание задачи 2", Status.NEW));
+        System.out.println("Созданные задачи:");
+        System.out.println(manager.getAllTasks());
+
+        task1.setStatus(Status.IN_PROGRESS);
+        manager.updateTask(task1);
+        System.out.println("Задача с обновленным статусом:");
+        System.out.println(manager.getTaskById(task1.getId()));
+
+        manager.deleteTaskById(task1.getId());
+        System.out.println("Список задач после удаления:");
+        System.out.println(manager.getAllTasks());
     }
 }
---
+++ src/Task.java
@@ -0,0 +1,74 @@
+import java.util.Objects;
+
+public class Task {
+    private int id;
+    private String name;
+    private String description;
+    private Status status;
+
+    public Task(int id, String name, String description, Status status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Task task = (Task) obj;
+        return id == task.id &&
+                Objects.equals(name, task.name) &&
+                Objects.equals(description, task.description) &&
+                Objects.equals(status, task.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+
+}
